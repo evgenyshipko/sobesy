c34f94d3176afd41226dcd71769e4cf516cf1782
Евгений Шипко <e.shipko@ritm.media>
2024-01-17 22:22:19 +0700

    group anagrams

---
+++ 4.group_anagram.ts
@@ -0,0 +1,23 @@
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ */
+
+const groupAnagrams = (strs) => {
+    const groups = {}
+
+    for(const str of strs){
+        const sorted = str.split('').sort().join('')
+        if(!groups[sorted]){
+            groups[sorted] = [str]
+        }else{
+            groups[sorted].push(str)
+        }
+    }
+
+    return Object.values(groups)
+};
